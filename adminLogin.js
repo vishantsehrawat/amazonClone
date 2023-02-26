0cfe19edd1c6f6dca735720733b9467d1fc56c5c
Vishant Sehrawat <vishant.96.sehrawat@gmail.com>
2023-02-26 21:14:29 +0530

    Add files via upload

---
+++ adminLogin.js
@@ -0,0 +1,43 @@
+let loginBtn = document.getElementById("adminLoginBtn");
+let emailInput = document.querySelector("#email");
+let passwordInput = document.querySelector("#password");
+let loginMessage = document.querySelector("#login-message")
+let datas=[]
+async function fetchAndRenderusers(){
+  try {
+    let resolved=await fetch(`https://63c822925c0760f69ac60c18.mockapi.io/users`);
+    let data=await resolved.json();
+    datas=data
+  } catch (error) {
+    console.log(error)
+  }
+}
+fetchAndRenderusers()
+loginBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let email = emailInput.value;
+  let password = passwordInput.value;
+
+  if (email == "") {
+    alert("Fill your email.");
+  } else if (password == "") {
+    alert("Fill the password.");
+  } 
+  function signin(datas){
+    let flag = false;
+    datas.forEach((ele)=>{
+      if(ele.email==email && ele.password==password){
+        flag =true;
+        alert("LOGIN - SUCCESSFUL");
+        window.location.href = "admin.html";
+        return
+      }
+    })
+    if(!flag) {
+      alert("Incorrect details")
+      return false
+    }
+  }
+  signin(datas)
+});
